6a9395ac67f4c104bd71cc274db3f491e4ba0026
Mack Ward <mackorone@gmail.com>
2022-07-09 12:23:35 -0400

    [skip ci] Improve test_spotify.py coverage

--- src/tests/test_spotify.py
+++ src/tests/test_spotify.py
@@ -9,7 +9,93 @@ import aiohttp
 
 from plants.unittest_utils import UnittestUtils
 from playlist_types import PublishedPlaylist, PublishedPlaylistID
-from spotify import RetryBudgetExceededError, Spotify
+from spotify import (
+    RequestFailedError,
+    ResponseType,
+    RetryableError,
+    RetryBudgetExceededError,
+    Spotify,
+)
+
+
+class TestSendRequestAndCoerceErrors(IsolatedAsyncioTestCase):
+    async def asyncSetUp(self) -> None:
+        self.aenter_to_send_request = AsyncMock()
+
+    async def test_invalid_access_token_error(self) -> None:
+        async with self.aenter_to_send_request as response:
+            response.status = 401
+        with self.assertRaises(RetryableError) as e:
+            await Spotify._send_request_and_coerce_errors(
+                aenter_to_send_request=self.aenter_to_send_request,
+                expected_response_type=ResponseType.JSON,
+                raise_if_request_fails=True,
+            )
+        self.assertEqual(e.exception.sleep_seconds, 1)
+        self.assertTrue(e.exception.refresh_access_token)
+
+    async def test_rate_limited_error(self) -> None:
+        async with self.aenter_to_send_request as response:
+            response.status = 429
+            response.headers = {"Retry-After": "2"}
+        with self.assertRaises(RetryableError) as e:
+            await Spotify._send_request_and_coerce_errors(
+                aenter_to_send_request=self.aenter_to_send_request,
+                expected_response_type=ResponseType.JSON,
+                raise_if_request_fails=True,
+            )
+        self.assertEqual(e.exception.sleep_seconds, 3)
+        self.assertFalse(e.exception.refresh_access_token)
+
+    async def test_unexpected_empty_response_error(self) -> None:
+        async with self.aenter_to_send_request as response:
+            response.status = 200
+            response.json.return_value = {}
+        # Problematic for expected_response_type=ResponseType.JSON
+        with self.assertRaises(RetryableError) as e:
+            await Spotify._send_request_and_coerce_errors(
+                aenter_to_send_request=self.aenter_to_send_request,
+                expected_response_type=ResponseType.JSON,
+                raise_if_request_fails=True,
+            )
+        self.assertEqual(e.exception.sleep_seconds, 1)
+        self.assertFalse(e.exception.refresh_access_token)
+        # No problems for expected_response_type=ResponseType.EMPTY
+        await Spotify._send_request_and_coerce_errors(
+            aenter_to_send_request=self.aenter_to_send_request,
+            expected_response_type=ResponseType.EMPTY,
+            raise_if_request_fails=True,
+        )
+
+    async def test_request_failed_error(self) -> None:
+        async with self.aenter_to_send_request as response:
+            response.status = 400
+            response.json.return_value = {"error": {"message": "foo"}}
+        # Problematic for raise_if_request_fails=True
+        with self.assertRaises(RequestFailedError) as e:
+            await Spotify._send_request_and_coerce_errors(
+                aenter_to_send_request=self.aenter_to_send_request,
+                expected_response_type=ResponseType.JSON,
+                raise_if_request_fails=True,
+            )
+        self.assertEqual(str(e.exception), "foo (400)")
+        # No problems for raise_if_request_fails=False
+        await Spotify._send_request_and_coerce_errors(
+            aenter_to_send_request=self.aenter_to_send_request,
+            expected_response_type=ResponseType.JSON,
+            raise_if_request_fails=False,
+        )
+
+    async def test_success(self) -> None:
+        async with self.aenter_to_send_request as response:
+            response.status = 201
+            response.json.return_value = {"foo": "bar"}
+        data = await Spotify._send_request_and_coerce_errors(
+            aenter_to_send_request=self.aenter_to_send_request,
+            expected_response_type=ResponseType.JSON,
+            raise_if_request_fails=True,
+        )
+        self.assertEqual(data, {"foo": "bar"})
 
 
 class MockSession(AsyncMock):
@@ -53,6 +139,12 @@ class SpotifyTestCase(IsolatedAsyncioTestCase):
             "spotify.Spotify._sleep",
             new_callable=AsyncMock,
         )
+        self.spotify = Spotify(
+            client_id="client_id",
+            client_secret="client_secret",
+            refresh_token="refresh_token",
+            retry_budget_seconds=10,
+        )
 
 
 class TestGetPlaylist(SpotifyTestCase):
@@ -71,40 +163,22 @@ class TestGetPlaylist(SpotifyTestCase):
         self.mock_session.get.return_value.__aenter__.side_effect = (
             aiohttp.client_exceptions.ClientOSError
         )
-        spotify = Spotify(
-            client_id="client_id",
-            client_secret="client_secret",
-            refresh_token="refresh_token",
-            retry_budget_seconds=10,
-        )
         with self.assertRaises(RetryBudgetExceededError):
-            await spotify._get_playlist(PublishedPlaylistID("abc123"))
+            await self.spotify._get_playlist(PublishedPlaylistID("abc123"))
 
     async def test_failed_request(self) -> None:
-        self.mock_session.get.return_value.__aenter__.return_value.json.return_value = {
-            "error": "error"
-        }
-        spotify = Spotify(
-            client_id="client_id",
-            client_secret="client_secret",
-            refresh_token="refresh_token",
-            retry_budget_seconds=10,
-        )
+        async with self.mock_session.get() as response:
+            response.json = {"error": "error"}
         with self.assertRaises(Exception):
-            await spotify._get_playlist(PublishedPlaylistID("abc123"))
+            await self.spotify._get_playlist(PublishedPlaylistID("abc123"))
 
     # Patch the logger to suppress log spew
     @patch("spotify.logger")
     async def test_server_unavailable(self, mock_logger: Mock) -> None:
-        self.mock_session.get.return_value.__aenter__.return_value.status = 500
-        spotify = Spotify(
-            client_id="client_id",
-            client_secret="client_secret",
-            refresh_token="refresh_token",
-            retry_budget_seconds=10,
-        )
+        async with self.mock_session.get() as response:
+            response.status = 500
         with self.assertRaises(RetryBudgetExceededError):
-            await spotify._get_playlist(PublishedPlaylistID("abc123"))
+            await self.spotify._get_playlist(PublishedPlaylistID("abc123"))
 
     # Patch the logger to suppress log spew
     @patch("spotify.logger")
@@ -122,14 +196,8 @@ class TestGetPlaylist(SpotifyTestCase):
             ),
         ]
         self.mock_session.get.return_value.__aenter__.side_effect = mock_responses
-        spotify = Spotify(
-            client_id="client_id",
-            client_secret="client_secret",
-            refresh_token="refresh_token",
-            retry_budget_seconds=10,
-        )
         playlist_id = PublishedPlaylistID("playlist_id")
-        playlist = await spotify._get_playlist(playlist_id)
+        playlist = await self.spotify._get_playlist(playlist_id)
         self.assertEqual(
             playlist,
             PublishedPlaylist(
@@ -162,14 +230,8 @@ class TestGetPlaylist(SpotifyTestCase):
             ),
         ]
         self.mock_session.get.return_value.__aenter__.side_effect = mock_responses
-        spotify = Spotify(
-            client_id="client_id",
-            client_secret="client_secret",
-            refresh_token="refresh_token",
-            retry_budget_seconds=10,
-        )
         playlist_id = PublishedPlaylistID("playlist_id")
-        playlist = await spotify._get_playlist(playlist_id)
+        playlist = await self.spotify._get_playlist(playlist_id)
         self.assertEqual(
             playlist,
             PublishedPlaylist(
@@ -185,12 +247,6 @@ class TestGetPlaylist(SpotifyTestCase):
 
 class TestShutdown(SpotifyTestCase):
     async def test_success(self) -> None:
-        spotify = Spotify(
-            client_id="client_id",
-            client_secret="client_secret",
-            refresh_token="refresh_token",
-            retry_budget_seconds=10,
-        )
-        await spotify.shutdown()
+        await self.spotify.shutdown()
         self.mock_session.close.assert_called_once()
         self.mock_sleep.assert_called_once_with(0)
